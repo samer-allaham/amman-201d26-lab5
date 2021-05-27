57f020cfb4019b2c86e70e8c51d431184a0f43a3
samer <s.allaham@ltuc.com>
2021-05-27 10:58:54 +0300

    initital commit

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
-  let sum=a+b;
-  return [sum,`The sum of ${a} and ${b} is ${sum}.`]
+
 }
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -29,12 +28,11 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) {
   //eslint-disable-line
-  let multiply=a*b;
-  return[multiply,`The product of ${a} and ${b} is ${multiply}.`]
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -73,18 +71,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
-  // "2,3,4 was passed in as an array of numbers, and 9 is their sum."
+
   //eslint-disable-line
-  let sumArray1=sum(sumArr[0],sumArr[1])[0];
-  let sumArray2=sum(sumArray1,sumArr[2])[0];
-
-  return[sumArray2,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumArray2} is their sum.`]
-
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -99,18 +92,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr,a=0,b=0,c=0) {
-  // "The numbers 2,3,4 have a product of 24."
+function multiplyArray(multArr) {
+  
   //eslint-disable-line
-  let multiply1=multiply(multArr[0],multArr[1])[0];
-  let multiply2=multiply(multiply1,multArr[2])[0];
-  console.log(multiply(multiply1,multArr[2]));
 
-  return[multiply2,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multiply2}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
